f8ebd23e95cccd480429ab41621e59fc19a28d2a
cormacpayne <corm@microsoft.com>
2019-10-02 14:34:56 -0700

    Update

--- index.js
+++ index.js
@@ -3,16 +3,19 @@ const github = require('@actions/github');
 
 try {
     const sourceDirectory = core.getInput('source-directory');
+    if (!sourceDirectory || sourceDirectory.length === 0) {
+        sourceDirectory = process.env.GITHUB_WORKSPACE
+    }
     const language = core.getInput('language');
-    console.log('Using source directory "${sourceDirectory}"');
-    console.log('Using language "${language}"');
+    console.log(`Using source directory "${sourceDirectory}"`);
+    console.log(`Using language "${language}"`);
     if (!language || language.length === 0) {
-        core.setOutput('temp', 'Running command "oryx build ${sourceDirectory}"');
+        core.setOutput('temp', `Running command "oryx build ${sourceDirectory}"`);
     } else {
-        core.setOutput('temp', 'Running command "oryx build ${sourceDirectory} --platform ${language}"');
+        core.setOutput(`temp', 'Running command "oryx build ${sourceDirectory} --platform ${language}"`);
     }
     const payload = JSON.stringify(github.context.payload, undefined, 2);
-    console.log('The event payload: ${payload}');
+    console.log(`The event payload: ${payload}`);
 } catch (error) {
     core.setFailed(error.message);
 }
