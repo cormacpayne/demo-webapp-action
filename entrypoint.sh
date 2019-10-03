d148851d268826fb2e645df0e944db14efc737c8
cormacpayne <corm@microsoft.com>
2019-10-03 09:27:50 -0700

    Bash variables

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,20 @@
 echo "Source directory: $1"
 echo "Language: $2"
 
-# echo $PWD
+if [ -n "$1" ]
+then
+    echo "Source directory provided: '$1'"
+else
+then
+    echo "No source directory provided."
+fi
+
+if [ -n "$2" ]
+then
+    echo "Language provided: '$2'"
+else
+then
+    echo "No language provided."
+fi
+
+echo $PWD
