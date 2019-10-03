4e77515d83f8ba15a3ac71642ba9247636e4ede1
cormacpayne <corm@microsoft.com>
2019-10-03 09:31:56 -0700

    String check

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,13 @@
 #!/bin/sh -l
 
-if [ -n $1 ]
+if [ -n "$1" ]
 then
     echo "Source directory provided: '$1'"
 else
     echo "No source directory provided."
 fi
 
-if [ -n $2 ]
+if [ -n "$2" ]
 then
     echo "Language provided: '$2'"
 else
