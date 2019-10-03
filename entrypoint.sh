58d00c3afe695179bc847b0c77b11f30f731d942
cormacpayne <corm@microsoft.com>
2019-10-03 09:29:47 -0700

    Bash fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,21 +1,16 @@
 #!/bin/sh -l
 
-echo "Source directory: $1"
-echo "Language: $2"
-
-if [ -n "$1" ]
+if [ -n $1 ]
 then
     echo "Source directory provided: '$1'"
 else
-then
     echo "No source directory provided."
 fi
 
-if [ -n "$2" ]
+if [ -n $2 ]
 then
     echo "Language provided: '$2'"
 else
-then
     echo "No language provided."
 fi
 
